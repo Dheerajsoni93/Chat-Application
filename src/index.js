de808703643c4f75a6085146ed8291dab4df071c
Dheeraj Soni <dheerajsoni7023@gmail.com>
2021-10-02 23:42:19 +0530

    add HTML pages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage } = require('./utils/messages')
+const { generateLocationMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -12,20 +15,39 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
-
 io.on('connection', (socket) => {
     console.log('New WebSocket Connection')
 
-    socket.emit('countUpdated', count)
+    socket.on('join', ({ username, room }) => {
+        socket.join(room)
+        
+        socket.emit('message', generateMessage('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMessage(username + ' has joined!'))
 
-    socket.on('increment', () => {
-        count++
-        io.emit('countUpdated', count)
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed!')
+        }
+
+        io.to(' ').emit('message', generateMessage(message))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessage(username + ' has left!'))
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('locationMessage', generateLocationMessage('https://google.com/maps?q='+ coords.latitude +',' + coords.longitude))
+        callback()
     })
 })
 
 server.listen(port, () => {
-    console.log('Server is up on port')
+    console.log('Server is up on port ' + port)
 })
 
